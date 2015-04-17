5473e279e354b0b3e386f4d84af79061b22a8506
Depthgr8 <depthgr8@gmail.com>
2015-04-17 20:48:56 +0530

    First commit - Application can convert point cloud files

---
+++ pcl-gui/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace pcl_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.convertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pLYToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oBJToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pCDToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.rAWToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pCDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pLYToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.vTKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pCDFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.convertToolStripMenuItem,
+            this.clipToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(530, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // convertToolStripMenuItem
+            // 
+            this.convertToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pLYToolStripMenuItem,
+            this.pCDToolStripMenuItem});
+            this.convertToolStripMenuItem.Name = "convertToolStripMenuItem";
+            this.convertToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.convertToolStripMenuItem.Text = "Convert";
+            // 
+            // pLYToolStripMenuItem
+            // 
+            this.pLYToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oBJToolStripMenuItem,
+            this.pCDToolStripMenuItem1,
+            this.rAWToolStripMenuItem});
+            this.pLYToolStripMenuItem.Name = "pLYToolStripMenuItem";
+            this.pLYToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pLYToolStripMenuItem.Text = "PLY File";
+            this.pLYToolStripMenuItem.Click += new System.EventHandler(this.pLYToolStripMenuItem_Click);
+            // 
+            // oBJToolStripMenuItem
+            // 
+            this.oBJToolStripMenuItem.Name = "oBJToolStripMenuItem";
+            this.oBJToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.oBJToolStripMenuItem.Text = "PLY to OBJ";
+            // 
+            // pCDToolStripMenuItem1
+            // 
+            this.pCDToolStripMenuItem1.Name = "pCDToolStripMenuItem1";
+            this.pCDToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.pCDToolStripMenuItem1.Text = "PLY to PCD";
+            // 
+            // rAWToolStripMenuItem
+            // 
+            this.rAWToolStripMenuItem.Name = "rAWToolStripMenuItem";
+            this.rAWToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.rAWToolStripMenuItem.Text = "PLY to RAW";
+            // 
+            // pCDToolStripMenuItem
+            // 
+            this.pCDToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pLYToolStripMenuItem1,
+            this.vTKToolStripMenuItem});
+            this.pCDToolStripMenuItem.Name = "pCDToolStripMenuItem";
+            this.pCDToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pCDToolStripMenuItem.Text = "PCD File";
+            // 
+            // pLYToolStripMenuItem1
+            // 
+            this.pLYToolStripMenuItem1.Name = "pLYToolStripMenuItem1";
+            this.pLYToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.pLYToolStripMenuItem1.Text = "PCD to PLY";
+            this.pLYToolStripMenuItem1.Click += new System.EventHandler(this.pLYToolStripMenuItem1_Click);
+            // 
+            // vTKToolStripMenuItem
+            // 
+            this.vTKToolStripMenuItem.Name = "vTKToolStripMenuItem";
+            this.vTKToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.vTKToolStripMenuItem.Text = "PCD to VTK";
+            // 
+            // clipToolStripMenuItem
+            // 
+            this.clipToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pCDFileToolStripMenuItem});
+            this.clipToolStripMenuItem.Name = "clipToolStripMenuItem";
+            this.clipToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.clipToolStripMenuItem.Text = "Clip";
+            // 
+            // pCDFileToolStripMenuItem
+            // 
+            this.pCDFileToolStripMenuItem.Name = "pCDFileToolStripMenuItem";
+            this.pCDFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pCDFileToolStripMenuItem.Text = "PCD File";
+            this.pCDFileToolStripMenuItem.Click += new System.EventHandler(this.pCDFileToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 367);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "pcl-gui";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem convertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pLYToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oBJToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pCDToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem rAWToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pCDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pLYToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem vTKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clipToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pCDFileToolStripMenuItem;
+    }
+}
+
---
+++ pcl-gui/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace pcl_gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pLYToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+            // Open file dialog window
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "pcd files (*.pcd)|*.pcd";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Exception handling
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            
+            // Processing of input and output file names
+            string in_file = openFileDialog1.FileName;
+            string out_file = in_file.Replace(".pcd",".ply");
+            out_file = "\"" + out_file + "\"";
+            in_file = "\"" + in_file + "\"";
+           
+            // Executing the program pcd2ply_release.exe
+            var process1 = new Process();
+            process1.StartInfo.FileName = @"C:\Program Files (x86)\PCL 1.6.0\bin\pcl_pcd2ply_release.exe"; // Path to your demo application.
+            process1.StartInfo.Arguments = String.Join(" ", in_file, out_file);
+            process1.Start();
+           
+        }
+
+        private void pLYToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pCDFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Open file dialog window
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "pcd files (*.pcd)|*.pcd";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Exception handling
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+            // Processing of input and output file names
+            string in_file = openFileDialog1.FileName;
+            string out_file = in_file.Replace(".pcd", ".ply");
+            out_file = "\"" + out_file + "\"";
+            
+            // Executing the program pcd2ply_release.exe
+            var process1 = new Process();
+            process1.StartInfo.FileName = @"C:\Users\Windows\Desktop\Work on PCL\build\Debug\point_clipper.exe"; // Path to your demo application.
+            process1.StartInfo.Arguments = String.Join(" ", in_file);
+            process1.Start();
+        }
+    }
+}
